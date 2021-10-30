033d9e092ec6d8a55a775f06ac6c2fe92b09a524
rob <robert_regan86@yahoo.com>
2021-10-29 20:23:33 -0400

    created working button and input

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById('input-btn').addEventListener('click', saveInput )
+
+let myLeads = []
+const inputEl = document.getElementById('input-el')
+
+
+
+function saveInput(){
+   myLeads.push('www.awesomelead.com')
+}
+
