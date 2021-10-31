647cfac18fd3a424e5dd602ad8bc3284239eaaae
rob <robert_regan86@yahoo.com>
2021-10-30 20:25:46 -0400

    added to extensions, prob halfway done

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 document.getElementById('input-btn').addEventListener('click', saveInput )
-
+const ulEl = document.getElementById('ul-el') 
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 
 
 
 function saveInput(){
-   myLeads.push('www.awesomelead.com')
+    myLeads.push(inputEl.value)
+    renderLead()
+    inputEl.value = ''
 }
+
+function renderLead(){
+    let listItems = ''
+    for(let i = 0; i < myLeads.length; i++){
+       listItems += `<li>
+                        <a href=${myLeads[i]} 
+                        target="_blank"> 
+                            ${myLeads[i]} 
+                        </a>
+                    </li>`
+    }
+    ulEl.innerHTML = listItems
+}
+
+
 
